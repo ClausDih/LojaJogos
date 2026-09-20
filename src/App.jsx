abf47451cbe5ee676173dbb96a20a9618373f6b3
ClausDih <claudiana.santos@sptech.school>
2026-09-20 16:15:42 -0300

    vitrine com as exigências da atividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import api from "./services/api";
 
 function App() {
   const [jogos, setJogos] = useState([])
+  const [erro, setErro] = useState('')
 
   useEffect(() => {
     api.get('/jogos')  /*aqui eu to fazendo a requisição com GET (servidor, me dê os jogos que estão lá no localhost 8080)*/
@@ -10,7 +11,7 @@ function App() {
       setJogos(resposta.data) /*mas a resposta vem dentro de um obj do axios, ou seja ele pega os dados q vieram do servidor e coloca na lista de jogos*/
     })
     .catch(() => {    /*aqui é a msg de erro caso o codigo acima, a requisição, falhe (é o meu else)*/
-      console.log('Erro ao buscar os jogos')
+      setErro('Não foi possível carregar os jogos')
     })
   }, [])   /*me mostre a lista quando o useEffect for montado*/
 
@@ -18,14 +19,18 @@ function App() {
     <>
     <h1>Loja de jogos</h1>
 
-    {jogos.map((jogo) => (    /*eu uso o .map  para ele passar por cada objeto da lista e colocar ele no formato abaixo q eu preciso. O key sabe qual é cada jogo pelo id, essa é a minha chave para classificação. qdo eu uso {}, to colocando javascript dentro do jsx pq é assim q se faz.*/
+    {erro ? (
+      <p>{erro}</p>
+    ) : (
+      jogos.map((jogo) => (    /*eu uso o .map  para ele passar por cada objeto da lista e colocar ele no formato abaixo q eu preciso. O key sabe qual é cada jogo pelo id, essa é a minha chave para classificação. qdo eu uso {}, to colocando javascript dentro do jsx pq é assim q se faz.*/
       <div key={jogo.id}>    
         <h2>{jogo.titulo}</h2>
         <p>Gênero: {jogo.genero}</p>
-        <p>Preço: {jogo.preco}</p>
-        <p>Estoque: {jogo.estoque}</p>
+        <p>Preço: {jogo.preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+        <p>Estoque: {jogo.estoque == 0 ? 'Esgotado': `${jogo.estoque}`}</p>
       </div>
-    ))}
+    ))
+  )}
     </>
   )
 }
